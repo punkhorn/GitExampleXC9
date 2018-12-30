be43004b0d2ac8dbd23690783743e20c0da519f5
William Collins <punkhornsw@gmail.com>
2018-12-30 11:54:47 -0500

    Initial Commit

---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by William Collins on 12/30/18.
+//  Copyright © 2018 Punkhorn Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
